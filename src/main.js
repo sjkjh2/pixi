cc9fdbd63830bcc104a4fed66e2aa088448fa4c6
sjkjh2 <sjkjh2@gmail.com>
2025-01-31 14:11:56 +0900

    pixi js 구조

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+import './style.css';
 import * as PIXI from 'pixi.js';
+
+(async() => {
+  // Application
+  const app = new PIXI.Application();
+
+  await app.init({
+    background: 'royalblue'
+  });
+
+  app.canvas.id = 'app-canvas';
+
+  document.body.appendChild(app.canvas);
+})();
+
